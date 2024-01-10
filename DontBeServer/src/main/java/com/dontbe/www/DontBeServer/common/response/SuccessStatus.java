baf37f0c589d70250d7e580b52d8c2ce6093265f
Hong0329 <97835512+Hong0329@users.noreply.github.com>
2024-01-10 17:41:06 +0900

    [FIX]CI 오류 해결

--- DontBeServer/src/main/java/com/dontbe/www/DontBeServer/common/response/SuccessStatus.java
+++ DontBeServer/src/main/java/com/dontbe/www/DontBeServer/common/response/SuccessStatus.java
@@ -25,8 +25,8 @@ public enum SuccessStatus {
     /**
      * comment
      */
-    POST_COMMENT_SUCCESS(HttpStatus.CREATED,"답글 작성 성공")
-    DELETE_CONTENT_SUCCESS(HttpStatus.OK,"게시물 삭제 성공"),
+    POST_COMMENT_SUCCESS(HttpStatus.CREATED,"답글 작성 성공"),
+    DELETE_CONTENT_SUCCESS(HttpStatus.OK,"게시물 삭제 성공")
     ;
 
     private final HttpStatus httpStatus;
