2494dab31fdc4a3917280c047815411907d9fca2
Hong0329 <rhkgkrwkhj@naver.com>
2024-01-10 15:25:40 +0900

    [FEAT]게시글 삭제 API개발

--- DontBeServer/src/main/java/com/dontbe/www/DontBeServer/api/content/controller/ContentController.java
+++ DontBeServer/src/main/java/com/dontbe/www/DontBeServer/api/content/controller/ContentController.java
@@ -1,7 +1,7 @@
 package com.dontbe.www.DontBeServer.api.content.controller;
 
 import com.dontbe.www.DontBeServer.api.content.dto.request.ContentPostRequestDto;
-import com.dontbe.www.DontBeServer.api.content.service.ContentQueryService;
+import com.dontbe.www.DontBeServer.api.content.service.ContentCommandService;
 import com.dontbe.www.DontBeServer.common.response.ApiResponse;
 import com.dontbe.www.DontBeServer.common.util.MemberUtil;
 import jakarta.validation.Valid;
@@ -17,12 +17,18 @@ import static com.dontbe.www.DontBeServer.common.response.SuccessStatus.*;
 @RequiredArgsConstructor
 @RequestMapping("api/v1")
 public class ContentController {
-    private final ContentQueryService contentQueryService;
+    private final ContentCommandService contentCommandService;
 
     @PostMapping("content")
     public ResponseEntity<ApiResponse<Object>> postContent(Principal principal, @Valid @RequestBody ContentPostRequestDto contentPostRequestDto) {
-        contentQueryService.postContent(MemberUtil.getMemberId(principal),contentPostRequestDto);
+        contentCommandService.postContent(MemberUtil.getMemberId(principal),contentPostRequestDto);
         return ApiResponse.success(POST_CONTENT_SUCCESS);
+    }
+
+    @DeleteMapping("content/{contentId}")
+    public ResponseEntity<ApiResponse<Object>> deleteContent(Principal principal, @PathVariable("contentId") Long contentId) {
+        contentCommandService.deleteContent(MemberUtil.getMemberId(principal),contentId);
+        return ApiResponse.success(DELETE_CONTENT_SUCCESS);
     }
 
 }
