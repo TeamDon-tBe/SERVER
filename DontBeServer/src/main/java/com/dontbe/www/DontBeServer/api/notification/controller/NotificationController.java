7af4f2083eadb1cf56d1921c6d9a308b976619b9
Hong0329 <rhkgkrwkhj@naver.com>
2024-01-14 04:29:44 +0900

    [FEAT]안읽은 노티 개수 조회 API

--- DontBeServer/src/main/java/com/dontbe/www/DontBeServer/api/notification/controller/NotificationController.java
+++ DontBeServer/src/main/java/com/dontbe/www/DontBeServer/api/notification/controller/NotificationController.java
@@ -1,10 +1,12 @@
 package com.dontbe.www.DontBeServer.api.notification.controller;
 
+import com.dontbe.www.DontBeServer.api.notification.dto.response.NotificaitonCountResponseDto;
 import com.dontbe.www.DontBeServer.api.notification.service.NotificationCommandService;
 import com.dontbe.www.DontBeServer.common.response.ApiResponse;
 import com.dontbe.www.DontBeServer.common.util.MemberUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,5 +26,11 @@ public class NotificationController {
         Long memberId = MemberUtil.getMemberId(principal);
         notificationCommandService.readNotification(memberId);
         return ApiResponse.success(READ_NOTIFICATION_SUCCESS);
+    }
+
+    @GetMapping("notification/number")
+    public ResponseEntity<ApiResponse<NotificaitonCountResponseDto>> countNotification(Principal principal) {
+        Long memberId = MemberUtil.getMemberId(principal);
+        return ApiResponse.success(COUNT_NOTIFICATION_SUCCESS, notificationCommandService.countUnreadNotification(memberId));
     }
 }
--- DontBeServer/src/main/java/com/dontbe/www/DontBeServer/api/notification/service/NotificationCommandService.java
+++ DontBeServer/src/main/java/com/dontbe/www/DontBeServer/api/notification/service/NotificationCommandService.java
@@ -1,20 +1,20 @@
 package com.dontbe.www.DontBeServer.api.notification.service;
 
-import com.dontbe.www.DontBeServer.api.content.dto.response.ContentGetAllByMemberResponseDto;
 import com.dontbe.www.DontBeServer.api.member.domain.Member;
 import com.dontbe.www.DontBeServer.api.member.repository.MemberRepository;
 import com.dontbe.www.DontBeServer.api.notification.domain.Notification;
+import com.dontbe.www.DontBeServer.api.notification.dto.response.NotificaitonCountResponseDto;
 import com.dontbe.www.DontBeServer.api.notification.repository.NotificationRepository;
-import com.dontbe.www.DontBeServer.common.util.TimeUtilCustom;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class NotificationCommandService {
     private final MemberRepository memberRepository;
     private final NotificationRepository notificationRepository;
@@ -24,5 +24,11 @@ public class NotificationCommandService {
         List<Notification> unreadNotifications = notificationRepository.findAllByNotificationTargetMemberAndIsNotificationChecked(targetMember,false);
 
         unreadNotifications.forEach(Notification::readNotification);
+    }
+
+    public NotificaitonCountResponseDto countUnreadNotification(Long memberId) {
+        Member member = memberRepository.findMemberByIdOrThrow(memberId);
+        int number = notificationRepository.countByNotificationTargetMemberAndIsNotificationChecked(member, false);
+        return NotificaitonCountResponseDto.of(number);
     }
 }
