3db50a954e28773196cebae498b9a9dbe8446530
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2024-02-20 02:02:59 +0900

    [del] print문 삭제

--- DontBeServer/src/main/java/com/dontbe/www/DontBeServer/api/member/service/MemberCommandService.java
+++ DontBeServer/src/main/java/com/dontbe/www/DontBeServer/api/member/service/MemberCommandService.java
@@ -55,7 +55,6 @@ public class MemberCommandService {
         List<Ghost> ghostList1 = ghostRepository.findByGhostTargetMember(member);
         List<Ghost> ghostList2 = ghostRepository.findByGhostTriggerMember(member);
 
-        System.out.println("들어옴");
         //게시글들 안에서 각 게시글에 대한 답글들의 좋아요 노티, 투명도 노티, 답글 노티, 답글 삭제 + 게시글 좋아요 삭제, 게시글 투명도 삭제, 게시글 삭제(소프트 딜리트 X)
         for(Content content : contentList) {
             Long contentId = content.getId();
@@ -66,35 +65,24 @@ public class MemberCommandService {
                 notificationRepository.deleteByNotificationTriggerTypeAndNotificationTriggerId("commentGhost",contentId);
                 notificationRepository.deleteByNotificationTriggerTypeAndNotificationTriggerId("comment", comment.getId());
             }
-            System.out.println("1");
             notificationRepository.deleteByNotificationTriggerTypeAndNotificationTriggerId("contentLiked",contentId);
-            System.out.println("2");
             notificationRepository.deleteByNotificationTriggerTypeAndNotificationTriggerId("contentGhost",contentId);
-            System.out.println("3");
             contentRepository.deleteById(contentId);
-            System.out.println("4");
         }
 
         //투명도 관련 운영 노티는 위에서 삭제되지 않기 때문에 아래 과정을 통해서 삭제되게끔 했습니다.
         //target일 경우랑 trigger일 경우, 두 가지 경우에 대해서 모두 진행한 이유는 혹시 몰라서입니다.
         List<Notification> notification1 = notificationRepository.findAllByNotificationTargetMember(member);
-        System.out.println("5");
         List<Notification> notification2 = notificationRepository.findAllByNotificationTriggerMemberId(memberId);
-        System.out.println("6");
+
         notificationRepository.deleteAll(notification1);
-        System.out.println("가나라다라");
         notificationRepository.deleteAll(notification2);
-        System.out.println("마바사");
 
         //초기 앱심사를 위해서 만든 탈퇴이기 때문에 탈퇴하는 유저가 누른 투명도에 대해 상대방을 회복 시킬지 말지에 대한 부분은 생각하지 않고
         //그냥 삭제되는 것으로 일단 진행하겠습니다. (p.s 이전에 카톡으로 추후에는 soft delete 적용하기로함!)
         //이대로 진행할 경우 어떤 유저는 평생 default값이 -1일 수도ㅜㅜ
         ghostRepository.deleteAll(ghostList1);
-        System.out.println("고스트리스트1");
         ghostRepository.deleteAll(ghostList2);
-
-        System.out.println("고스트리스트2");
-//        contentRepository.deleteAll(contentList);
 
         //좋아요 삭제
         List<ContentLiked> contentLiked = contentLikedRepository.findAllByMemberId(memberId);
@@ -104,7 +92,6 @@ public class MemberCommandService {
 
         //탈퇴하는 유저가 작성한 답글 삭제
         commentRepository.deleteCommentsByMemberId(memberId);
-
 
         memberRepository.delete(member);
     }
